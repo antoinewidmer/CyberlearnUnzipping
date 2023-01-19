8e0f3d4a65e3ca90be4cc15bebf137084fb23c2f
Antoine Widmer <widmera@gmail.com>
2023-01-19 15:11:35 +0100

    Add project files.

---
+++ CyberlearnUnzipping/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace CyberlearnUnzipping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPath = @"C:\tests\6231";
+
+            var dirs = Directory.GetDirectories(startPath);
+            foreach(string dir in dirs)
+            {
+                string folder = dir.Substring(0, dir.LastIndexOf(("\\")));
+
+                var fileName = dir.Substring(dir.LastIndexOf(("\\")) + 1);
+
+                var names = fileName.Split(" ");
+                var lastname = names[0];
+                var lastnames = names[1].Split("_");
+                var firstname = lastnames[0];
+                var filenameOut = lastname + firstname;
+                var files = Directory.GetFiles(dir);
+
+                foreach (string f in files)
+                {
+                    var ext = Path.GetExtension(f);
+                    if (ext.Contains(".zip"))
+                    {
+                        ZipFile.ExtractToDirectory(f, startPath + "\\" + filenameOut);
+                        //Directory.Delete(dir, true);
+                    }
+
+                    else
+                        Console.WriteLine(firstname + " " + lastname);
+                }
+                
+                
+
+
+            }
+
+
+            
+        }
+    }
+}
