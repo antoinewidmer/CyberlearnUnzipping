b4b687229c12229a214a8cf7cf2a82e697e7e56b
Antoine Widmer <widmera@gmail.com>
2023-01-29 09:46:14 +0100

    modified lastname firstname order to take account of changes on the name of files in cyberlearn

--- CyberlearnUnzipping/Program.cs
+++ CyberlearnUnzipping/Program.cs
@@ -9,7 +9,8 @@ namespace CyberlearnUnzipping
     {
         static void Main(string[] args)
         {
-            string startPath = @"C:\tests\6231";
+            string startPath = @"C:\Users\antoine.widmer\Downloads\Module 634-1-ExamenModuleJanvier2023-1952378";
+            string toPath = @"C:\git\unzipped";
 
             var dirs = Directory.GetDirectories(startPath);
             foreach(string dir in dirs)
@@ -19,10 +20,10 @@ namespace CyberlearnUnzipping
                 var fileName = dir.Substring(dir.LastIndexOf(("\\")) + 1);
 
                 var names = fileName.Split(" ");
+                var firstname = names[1];
+                var firstnames = names[1].Split("_");
                 var lastname = names[0];
-                var lastnames = names[1].Split("_");
-                var firstname = lastnames[0];
-                var filenameOut = lastname + firstname;
+                var filenameOut = lastname + "_" +firstnames[0];
                 var files = Directory.GetFiles(dir);
 
                 foreach (string f in files)
@@ -30,7 +31,7 @@ namespace CyberlearnUnzipping
                     var ext = Path.GetExtension(f);
                     if (ext.Contains(".zip"))
                     {
-                        ZipFile.ExtractToDirectory(f, startPath + "\\" + filenameOut);
+                        ZipFile.ExtractToDirectory(f, toPath + "\\" + filenameOut);
                         //Directory.Delete(dir, true);
                     }
 
